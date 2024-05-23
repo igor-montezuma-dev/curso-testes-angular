8a41db92401ce1812b0d1d68be90cbcd6b37bb3a
IgorMontezuma20 <igorminranda17@gmail.com>
2024-05-23 15:30:05 -0300

    test: Fix render title unity test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tests-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Curso Testes Unitários com Angular / Jasmine e karma');
   });
 });
